90fb5330ea1469bca550833e7541920099f6bca8
nomadlife_15w <joonwoo3816@gmail.com>
2019-10-23 18:58:38 +0900

    first commit(all)

---
+++ app/src/main/java/com/example/test_tab_bottom/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.test_tab_bottom;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.test_tab_bottom.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
